f46114d4ede83723b7f110c9ed998e56b52bd99b
cjixuan <jiaxuan.cheng@sg.panasonic.com>
2025-02-14 11:02:15 +0800

    Added videos
    Swapped to using videos for tour creation process

--- public/script.js
+++ public/script.js
@@ -304,29 +304,66 @@ async function makeRequest(method, url, params) {
     });
 }
 
-var premadeVideo_element = document.getElementById('AI-tour-video');
+// Buffer to store and track which video element is being played
+var playingVideo = null;
 
-function swapToVideo() {
-    wrapper.classList.add("hiddenVideo"); // Hide Avatar
-    premadeVideo_element.style.width = "100vw";
-    premadeVideo_element.style.height = "100vh";
-    playVideo(premadeVideo_element);
+var tourVideo_element       = document.getElementById('AI-tour-video');
+var introVideo_element      = document.getElementById('AI-intro-video');
+var visitedVideo_element    = document.getElementById('AI-visited-video');
+var agegroupVideo_element   = document.getElementById('AI-agegroup-video');
+var interestsVideo_element  = document.getElementById('AI-interests-video');
+var durationVideo_element   = document.getElementById('AI-duration-video');
+var resultsVideo_element    = document.getElementById('AI-results-video');
+var idleVideo_element       = document.getElementById('AI-idle-video');
 
-    // When video ends, reset wrapper size and hide video
-    premadeVideo_element.addEventListener("ended", () => {
-    wrapper.classList.remove("hiddenVideo"); // Show Avatar
-    premadeVideo_element.style.width = "0";
-    premadeVideo_element.style.height = "0";
-    });
+function init()
+{
+  //Load all the video elements
+  tourVideo_element.load();
+  introVideo_element.load();
+  visitedVideo_element.load();
+  agegroupVideo_element.load();
+  interestsVideo_element.load();
+  durationVideo_element.load();
+  resultsVideo_element.load();
+  idleVideo_element.load();
+
+  //Pause all the video elements
+  pauseVideo(tourVideo_element);
+  pauseVideo(introVideo_element);
+  pauseVideo(visitedVideo_element);
+  pauseVideo(agegroupVideo_element);
+  pauseVideo(interestsVideo_element);
+  pauseVideo(durationVideo_element);
+  pauseVideo(resultsVideo_element);
+  pauseVideo(idleVideo_element);
+
+  //Play idle video
+  playVideo(idleVideo_element);
 }
 
-function playVideo(videoToPlay) {
-  //Play next video + resize
-  if(videoToPlay != null) {
-    videoToPlay.play();
-    videoToPlay.style.width = "100%";
-    videoToPlay.style.height = "100%";
+// Plays a video and sets the size of the video element to the AvatarSize
+function playVideo(videoToPlay)
+{
+  //Check if has current video
+  //If yes => pause that video + resize
+  if(playingVideo != null)
+  {
+    pauseVideo(playingVideo);
   }
+  //If no => do nothing
+
+  //Play next video + resize
+  if(videoToPlay != null)
+  {
+    wrapper.classList.add("hiddenVideo"); // Hide Avatar
+    videoToPlay.play();
+    videoToPlay.style.width = "100vw";
+    videoToPlay.style.height = "100vh";
+  }
+
+  //Update current playing video
+  playingVideo = videoToPlay;
 }
 
 // Pause a video and reduce it's size to 0px
@@ -338,3 +375,50 @@ function pauseVideo(videoToPause) {
     videoToPause.style.height = '0px';
   }
 }
+
+function showAvatar() {
+  //Pause all the video elements
+  pauseVideo(tourVideo_element);
+  pauseVideo(introVideo_element);
+  pauseVideo(visitedVideo_element);
+  pauseVideo(agegroupVideo_element);
+  pauseVideo(interestsVideo_element);
+  pauseVideo(durationVideo_element);
+  pauseVideo(resultsVideo_element);
+  pauseVideo(idleVideo_element);
+  wrapper.classList.remove("hiddenVideo"); // Show Avatar
+}
+
+init();
+ 
+tourVideo_element.addEventListener('ended', () => {
+  playVideo(idleVideo_element)
+})
+
+introVideo_element.addEventListener('ended', () => {
+  playVideo(idleVideo_element)
+})
+
+visitedVideo_element.addEventListener('ended', () => {
+  playVideo(idleVideo_element)
+})
+
+agegroupVideo_element.addEventListener('ended', () => {
+  playVideo(idleVideo_element)
+})
+
+interestsVideo_element.addEventListener('ended', () => {
+  playVideo(idleVideo_element)
+})
+
+durationVideo_element.addEventListener('ended', () => {
+  playVideo(idleVideo_element)
+})
+
+resultsVideo_element.addEventListener('ended', () => {
+  playVideo(idleVideo_element)
+})
+
+idleVideo_element.addEventListener('ended', () => {
+  playVideo(idleVideo_element)
+})
